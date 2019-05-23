653a414ee19d53a6a66bbf39194b97d53ca02b7b
UsuariosCAU <UsuariosCAU@cnbv.gob.mx>
2019-05-23 09:42:07 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use( require('../controladores/usuario') );
+
+mongoose.connect(process.env.URLDB,
+                {useNewUrlParser: true, useCreateIndex: true}, 
+                (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
+});
+
+app.listen(process.env.PORT, () => {
+
+    console.log(`Escuchando el puerto: `, process.env.PORT)
+
+});
